f311025feea981f10e0d64f6cededcfb05f97be4
SIgnacioRoccuzzo <129063696+SIgnacioRoccuzzo@users.noreply.github.com>
2024-04-11 18:54:28 +0200

    navbar

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  firstMenu = [
+    { label: 'Fibra y Movil', route: 'fibra-y-movil' },
+    { label: 'Fibra', route: 'fibra' },
+    { label: 'Tarifa Movil', route: 'movil' },
+    { label: 'Tv', route: 'tv' },
 
+  ];
+  secondMenu = [
+    { label: 'Sobre Epica', route: 'sobre-epica' },
+    { label: 'Ayuda', route: 'ayuda' },
+    { label: 'Contacto', route: 'contacto' },
+  ]
+  isMenuOpen = false;
+
+  constructor(private router: Router) { }
+
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  scrollTo(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+
+
+      this.router.navigate([], { fragment: sectionId });
+    }
+  }
 }
